01fa9c43b5a1d02d007664777e34431df830be9b
eng-mohamed-eldeeb <agnabiieldeeeb@gmail.com>
2024-03-06 20:06:10 +0200

    fix: fix if statment syntax error

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -15,6 +15,10 @@ const auth = async (req, res, next) => {
       req.user = { userId: payload.userId };
       // check if user has paid
       const user = await User.findById(req.user.userId);
+      if (user.role !== "admin") {
+        console.log(
+          user.getExpirationDate() < new Date() || user.package_type == "hold"
+        );
         if (
           user.getExpirationDate() < new Date() ||
           (user.package_type == "hold" &&
